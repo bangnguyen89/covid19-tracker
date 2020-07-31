c3477e4253933488c88fce807b6c6286f9bd14f1
duc.bang <nguyenducbang2018@gmail.com>
2020-07-31 22:15:03 +0700

    Updates

---
+++ precache-manifest.c8d1be779e71189354a16e74657b8c91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48aa13786b25a0f5dd7ed6f43b49f94",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "992ae32e733d365e058d",
+    "url": "/covid19-tracker/static/css/2.b756e824.chunk.css"
+  },
+  {
+    "revision": "223768244655ee4b3924",
+    "url": "/covid19-tracker/static/css/main.c5a14cee.chunk.css"
+  },
+  {
+    "revision": "992ae32e733d365e058d",
+    "url": "/covid19-tracker/static/js/2.8ad2b327.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19-tracker/static/js/2.8ad2b327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223768244655ee4b3924",
+    "url": "/covid19-tracker/static/js/main.b3b8e750.chunk.js"
+  },
+  {
+    "revision": "efb4724b6063bf7d0ee6",
+    "url": "/covid19-tracker/static/js/runtime-main.9faf7677.js"
+  }
+]);
